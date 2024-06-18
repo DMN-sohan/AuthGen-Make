5a79e5fb08cb689deb5758e23b7dbc46d2ebdbf6
DMN-sohan <65285526+DMN-sohan@users.noreply.github.com>
2024-06-19 04:14:50 +0530

    Update ai-server-make.py for Render

--- ai-server-make.py
+++ ai-server-make.py
@@ -80,4 +80,5 @@ async def make_image(file: UploadFile, secret: str = Form(...)):
     return FileResponse(file_path, media_type='image/png', filename='out.png')
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=7500)
+    port = int(os.getenv('PORT', 8000))
+    uvicorn.run(app, host='0.0.0.0', port=port)
